1be1a661ab8e56c6f40002d0fb3fce48d971e192
jfmemon <jannatul15-2497@diu.edu.bd>
2023-02-05 12:00:16 +0600

    level 4 is done

--- practice-level-4/level-4.js
+++ practice-level-4/level-4.js
@@ -1,61 +1,79 @@
+// for name
 const sendName = () => {
     const nameInputField = document.getElementById('name-input-field');
     const nameInputFieldText = nameInputField.value;
-    const savedValue = localStorage.setItem('Name',nameInputFieldText);
+    localStorage.setItem('Name',nameInputFieldText);
     nameInputField.value = ``;
-    return savedValue;
 }
 const deleteName = () => {
     localStorage.removeItem('Name');
 }
 
+// for email
 const sendEmail = () => {
     const emailInputField = document.getElementById('email-input-field');
     const emailInputFieldText = emailInputField.value;
-    const savedValue = localStorage.setItem('Email', emailInputFieldText);
+    localStorage.setItem('Email', emailInputFieldText);
     emailInputField.value =``;
-    return savedValue;
 }
 
 const deleteEmail = () => {
     localStorage.removeItem('Email');
 }
 
+// for message
 const sendMessage = () => {
     const messageInputField = document.getElementById('message-input-field');
     const messageInputFieldText = messageInputField.value;
-    const savedValue = localStorage.setItem('Message', messageInputFieldText);
+    localStorage.setItem('Message', messageInputFieldText);
     messageInputField.value =``;
-    return savedValue;
 }
 
 const deleteMessage = () => {
     localStorage.removeItem('Message');
 }
 
+// for reset
 const reset = () => {
     localStorage.clear();
 }
 
-// const getItemFromLocalStorage = () => {
-//     const savedValue = localStorage.getItem('Object');
-//     let object = {};
-//     if(savedValue) {
-//         object = JSON.parse(savedValue);
-//     }
-//     return object;
-// }
-
-// const setItemIntoLocalStorage = (key, value) => {
-//     const object = getItemFromLocalStorage();
-//     object[key] = value;
-//     const stringifyData = JSON.stringify('object')
-//     localStorage.setItem(object, stringifyData);
-// }
-
-const save = () => {
-    const nameData = sendName();
-    const emailData = sendEmail();
-    const messageData = sendMessage();
+const getItemFromLocalStorage = () => {
+    const savedValue = localStorage.getItem('Object');
+    let object = {};
+    if(savedValue) {
+        object = JSON.parse(savedValue);
+    }
+    return object;
 }
 
+const setItemIntoLocalStorage = (name, email, message) => {
+    const createdObject = getItemFromLocalStorage();
+    const savedName = localStorage.getItem(name);
+    if(savedName === null) {
+        localStorage.removeItem(savedName);
+    }else{
+        createdObject[name] = savedName;
+    }
+
+    const savedEmail = localStorage.getItem(email);
+    if(savedEmail === null) {
+        localStorage.removeItem(savedEmail);
+    }else{
+        createdObject[email] = savedEmail;
+    }
+
+    const savedMessage = localStorage.getItem(message);
+    if(savedMessage === null) {
+        localStorage.removeItem(savedMessage);
+    }else{
+        createdObject[message] = savedMessage;
+    }
+
+    const stringifiedObject = JSON.stringify(createdObject);
+    localStorage.setItem('Object', stringifiedObject);
+}
+
+const save = () => {
+    setItemIntoLocalStorage('Name', 'Email', 'Message');
+}
